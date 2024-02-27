83e660be527602fb04c68e1eac578aa8c5277ed7
FedericoGre <federico.garea@gmail.com>
2024-02-27 15:06:26 -0300

    SunSound Landing

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,31 @@
+import './Footer.css'
+
+function Footer() {
+  return (
+    <footer>
+    <div className="rectangle-div" />
+    <div className="landing-sunsound-child1" />
+    <div className="redes-sociales">Redes sociales</div>
+    <div className="acerca-de">Acerca de</div>
+    <div className="ingresa">Ingresa</div>
+    <div className="twitter">Twitter</div>
+    <div className="solana">Solana</div>
+    <div className="plataforma">Plataforma</div>
+    <div className="introduccin">Introducción</div>
+    <div className="instagram">Instagram</div>
+    <div className="facebook">Facebook</div>
+    <div className="tiktok">TikTok</div>
+    <div className="discord">Discord</div>
+    <h3 className="sunsound1">
+      <span className="sun">Sun</span>
+      <span className="sound">Sound</span>
+    </h3>
+    <div className="sunsound2">
+      <span className="sun">Sun</span>
+      <span className="sound">Sound ®</span>
+    </div>
+    </footer>
+  );
+}
+
+export default Footer;
